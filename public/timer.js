a7a6121e43bee497cc8f14966718220b7419debc
hyphensable <yuitosakina1131@gmail.com>
2022-08-30 23:16:14 +0900

    position取得

--- public/timer.js
+++ public/timer.js
@@ -1,5 +1,8 @@
 'use strict';
 {
+    import {getAuth} from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
+    const auth = getAuth();
+
     var time = 0;
     var timerLabel = document.getElementsByClassName('timerLabel')[0];
     var startBtn = document.getElementsByClassName('sampleButton-ok')[0];
@@ -10,6 +13,53 @@
 
     // STARTボタン
     function start() {
+        if (time == 0) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                //緯度
+                let lat = position.coords.latitude;
+                //経度
+                let lng = position.coords.longitude;
+            });
+            const response = await fetch("/position", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    "uid":auth.uid,
+                    "position":
+                    {
+                        "lat":lat,//緯度
+                        "lng":lng//経度
+                    },
+                    "time":timerLabel,
+                    "status":"start"
+                })
+
+              });
+        }
+
+        else if (time % 3000 == 0) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                //緯度
+                let lat = position.coords.latitude;
+                //経度
+                let lng = position.coords.longitude;
+            });
+            const response = await fetch("/position", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    "uid":auth.uid,
+                    "position":
+                    {
+                        "lat":lat,//緯度
+                        "lng":lng//経度
+                    },
+                    "time":timerLabel,
+                    "status":"running"
+                })
+
+              });
+        }
         // timeをsetTimeoutで設定したミリ秒ごとに1プラスする
         time++;
 
@@ -33,7 +83,27 @@
     function stop() {
         // 停止する
         clearTimeout(id);
+        navigator.geolocation.getCurrentPosition((position) => {
+            //緯度
+            let lat = position.coords.latitude;
+            //経度
+            let lng = position.coords.longitude;
+        });
+        const response = await fetch("/position", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                "uid": auth.uid,
+                "position":
+                {
+                    "lat": lat,//緯度
+                    "lng": lng//経度
+                },
+                "time": timerLabel,
+                "status": "finish"
+            })
 
+        });
     }
 
     function click() {
@@ -52,4 +122,5 @@
     // クリックした時の処理
     button.addEventListener('click', click)
     resetBtn.addEventListener('click', reset); // RESETボタン
+
 }
